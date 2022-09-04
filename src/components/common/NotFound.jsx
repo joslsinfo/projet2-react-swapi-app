0ea8143cbb73bc3e2ea5ede515c4b9dfb4bf5251
Jean Oldor Saintus <josaintus@gmail.com>
2022-09-04 13:38:19 -0400

    Ajout de la page NotFound.jsx pour le message 404

--- src/components/common/NotFound.jsx
+++ src/components/common/NotFound.jsx
@@ -4,8 +4,9 @@ import Container from "react-bootstrap/Container";
 const NotFound = () => {
   return (
     <Container fluid>
-      <Container className="vh-100 d-flex justify-content-center align-items-center">
-        <h1>404!</h1>
+      <Container className="vh-100 d-flex justify-content-center align-items-center text-danger">
+        <h1 className="gy-3">Error 404! The page you are looking for doesn't exist.</h1> 
+        
       </Container>
     </Container>
   );
